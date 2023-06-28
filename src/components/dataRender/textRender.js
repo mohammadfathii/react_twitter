a6c5ec5ba7ed92b18d3a917a2d202640932e4b7b
Mohammad <m_fathi65@yahoo.com>
2023-06-28 14:42:26 +0330

    change renderText method to better performance for application
    Signed-off-by: Mohammad <m_fathi65@yahoo.com>

--- src/components/dataRender/textRender.js
+++ src/components/dataRender/textRender.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
 const TextRender = (text) => {
-    let words = text.split(" ")
-    let newWords = words.map((word) => {
-        if(word.includes("#")){
-            word = `<a href="/tag/${word.replace('#','')}">${word}</a>`;
-            return word;
-        }else{
-            return word;
-        }
-    })
-    console.log(newWords)
-    return newWords.join(" ");
+    // let words = text.split(" ")
+    // let newWords = words.map((word) => {
+    //     if(word.includes("#")){
+    //         word = `<a href="/tag/${word.replace('#','')}">${word}</a>`;
+    //         return word;
+    //     }else{
+    //         return word;
+    //     }
+    // })
+    // console.log(newWords)
+    // return newWords.join(" ");
+
+    let newText = text.replace(/#\S+/g,"<a href={$&}>$&</a>")
+    return newText;
 };
 
 export default TextRender;
--- src/data/tweets.js
+++ src/data/tweets.js
@@ -1,5 +1,5 @@
 export  const tweetsList = [
-    {fullname : "محمد فتحی",username : 'mohammadfathi',tweet : 'به نام خدا #تست هستم'},
+    {fullname : "محمد فتحی",username : 'mohammadfathi',tweet : ' #تست به نام خدا #تست هستم'},
     {fullname : "محمد فتحی",username : 'mohammadfathi',tweet : 'توییت تست '},
     {fullname : "محمد فتحی",username : 'mohammadfathi',tweet : 'توییت تست '},
     {fullname : "محمد فتحی",username : 'mohammadfathi',tweet : 'توییت تست '},
